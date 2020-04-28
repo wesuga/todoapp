ad07e64cfb34c53d93c9b0b2119fae88b0a67aa4
Wesley Ugang <wesleygang09@gmail.com>
2020-04-28 23:41:36 +0800

    Add login filter

---
+++ src/com/todolist/controller/LoginFilter.java
@@ -0,0 +1,32 @@
+package com.todolist.controller;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter({ "/AddTodoController", "/DeleteTodoController", "/ListTodoController", "/LoadTodoController",
+	"/UpdateTodoController" })
+public class LoginFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	    throws IOException, ServletException {
+	HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+	if (httpRequest.getSession().getAttribute("username") != null) {
+	    chain.doFilter(request, response);
+	} else {
+	    httpRequest.getRequestDispatcher("LoginController").forward(request, response);
+	}
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+}
